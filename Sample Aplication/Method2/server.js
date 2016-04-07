0d7d728496428718c7da70ed56aa53b7df433286
Matías Abal <abalheviamatias@gmail.com>
2016-04-08 00:01:57 +0100

    Method 2: Using Express

--- Sample Aplication/Method2/server.js
+++ Sample Aplication/Method2/server.js
@@ -1,10 +1,10 @@
-var http = require("http"),
-  fs = require ("fs");
+var express = require("express");
+var app = express();
+var path = require("path");
 
-http.createServer(function (req, res){
-  res.writeHead(20, {"Content-Type": "text/html", "Access-Control-Allow-Origin" : "*"});
-  var readStream = fs.createReadStream(__dirname + "/index.html");
-  readStream.pipe(res);
-}).listen(1337);
+app.get("/", function(req, res){
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
 
+app.listen(1337);
 console.log("Visit me at http://localhost:1337");
