383a2ac4e496cc37e35bb6fe090288ef639efd92
matiasah7 <abalheviamatias@gmail.com>
2016-04-12 18:57:40 +0100

    AngularJS App Finished

---
+++ routing-app/public/js/app.js
@@ -0,0 +1,17 @@
+angular.module("routerApp", ["routerRoutes"])
+  .controller("mainController", function(){
+    var vm = this;
+    vm.bigMessage = "A smooth sea never made a skilled sailor.";
+  })
+  .controller("homeController", function(){
+    var vm = this;
+    vm.message = "This is the home page!";
+  })
+  .controller("aboutController", function(){
+    var vm = this;
+    vm.message = "Look! I am an about page.";
+  })
+  .controller("contactController", function(){
+    var vm = this;
+    vm.message = "Contact us! JK. This is just a demo.";
+  });
