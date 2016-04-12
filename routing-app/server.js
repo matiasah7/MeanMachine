a7af09734d8997de5ca4ebdfe46720528a1b90ce
Matías Abal <abalheviamatias@gmail.com>
2016-04-12 22:57:26 +0100

    no message

--- routing-app/server.js
+++ routing-app/server.js
@@ -1,11 +1,11 @@
 var express = require("express"),
-  app = express(),
-  path = require("path");
+    app = express(),
+    path = require("path");
 
 app.use(express.static(__dirname + "/public"));
 
-app.get("*", function(req, res){
-  res.sendFile(path.join(__dirname + "/public/views/index.html"));
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname + "/public/views/index.html"));
 });
 
 app.listen(8080);
