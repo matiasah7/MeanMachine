dcf0b1483f2c3008ff93529d2549c1a65dfe582a
matiasah7 <abalheviamatias@gmail.com>
2016-04-15 12:41:30 +0100

    Front-End Services added (Auth and User)

--- App-Structure/public/app/services/userService.js
+++ App-Structure/public/app/services/userService.js
@@ -1,9 +1,26 @@
-angular.module("stuffService", [])
-    .factory("Stuff", function($http) {
-        var myFactory = {};
-        myFactory.all = function() {
-            return $http.get("/api/stuff");
+angular.module("userService", [])
+    .factory("User", function($http) {
+        var userFactory = {};
+
+        userFactory.get = function(id) {
+            return $http.get("/api/users/" + id);
         };
 
-        return myFactory;
+        userFactory.all = function() {
+            return $http.get("/api/users/");
+        };
+
+        userFactory.create = function(userData) {
+            return $http.post("/api/users/", userData);
+        };
+
+        userFactory.update = function(id, userData) {
+            return $http.put("/api/users/" + id, userData);
+        };
+
+        userFactory.delete = function(id) {
+            return $http.delete("/api/users/" + id);
+        };
+
+        return userFactory;
     });
