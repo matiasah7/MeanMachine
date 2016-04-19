b421345f20cb163eb1682b406b86a7505f3463df
matiasah7 <abalheviamatias@gmail.com>
2016-04-19 15:05:11 +0100

    Index done!

--- Mean App Users Management CRM/server.js
+++ Mean App Users Management CRM/server.js
@@ -33,7 +33,7 @@ app.use("/api", apiRoutes);
 
 app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname + "/public/app/views/index.html"));
-})
+});
 
 app.listen(config.port);
 console.log("Magic happens on port " + config.port);
