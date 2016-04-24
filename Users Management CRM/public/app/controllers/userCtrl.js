810cc9715af867fedd7d73c13d862eef8fdcde2f
Matías Abal <abalheviamatias@gmail.com>
2016-04-24 21:14:02 +0100

    delete user added!

--- Users Management CRM/public/app/controllers/userCtrl.js
+++ Users Management CRM/public/app/controllers/userCtrl.js
@@ -8,4 +8,16 @@ angular.module('userCtrl', ['userService'])
         vm.processing = false;
         vm.users = data;
       });
+
+    vm.deleteUser = function(id){
+      vm.processing = true;
+      User.delete(id)
+        .success(function(data){
+          User.all()
+            .success(function(data){
+              vm.processing = false;
+              vm.users = data;
+            });
+        });
+    };
   });
